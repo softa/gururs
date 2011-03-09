d11818e270a074afd94feb16543fb26e7e0582ff
Leonardo Tartari <ltartari@gmail.com>
2011-03-09 20:56:22 -0300

    design rc

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,8 @@
 $(function(){
   // sets the LAST li
   $('#main ul').find('li:last-child').addClass('last');
+  // wow
+  $('#dojos-list span, #dojos-list time').animate({
+    top: '-=40'
+  }, 750, function() {});
 });
